f9c36235ed1c548999e2d0563b791c94fb559949
Alex Z <alex3181@yahoo.com>
2023-05-18 10:49:01 -0400

    completed air quality file

--- air-quality-tester.py
+++ air-quality-tester.py
@@ -2,3 +2,14 @@ from time import sleep
 from Adafruit_CCS811 import Adafruit_CCS811
 
 css = Adafruit_CCS811()
+
+import busio
+import adafruit_ccs811
+
+from board import *
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+
+ccs = adafruit_ccs811.CCS811(i2c)
+
+print("CO2: ", ccs.eco2, " TVOC:", ccs.tvoc)
