bbd80a5e8ecec7c7ce81d9eb5dc541ae9edaea7b
Alex <alex3181@yahoo.com>
2024-09-26 15:34:31 -0400

    9/26/24

--- qt-display.py
+++ qt-display.py
@@ -489,7 +489,7 @@ class window(QWidget):
 def main():
     app = QApplication(sys.argv)
     ex = window()
-    # ex.showFullScreen()  # show fullscreen
+    ex.showFullScreen()  # show fullscreen
     ex.show()
     sys.exit(app.exec_())
 
